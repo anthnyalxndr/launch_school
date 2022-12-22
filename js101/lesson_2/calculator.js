747232d7b6b95c0c4ca73d76a6f48d72b7b168a1
anthnyalxndr <58750392+anthnyalxndr@users.noreply.github.com>
2022-12-22 12:13:35 -0800

    Updates calculator.js w/ bonus features
    Bonus features:
    1) Added the option to do another calculation by prompting user
    2) Configured a json file and imported it to make the program more modular by aliasing repeating messages.

--- js101/lesson_2/calculator.js
+++ js101/lesson_2/calculator.js
@@ -3,58 +3,65 @@
  * & divide 2 numbers provided by user input.
  */
 
+let another = 'yes';
+let MESSAGES = require('./calculator_messages.json');
 
-let result;
+while (another === 'yes') {
+  let result;
 
-let rlSync = require('readline-sync');
+  let rlSync = require('readline-sync');
 
-const prompt = (message) => {
-  console.log('=> ' + message);
-};
+  const prompt = (message) => {
+    console.log('=> ' + message);
+  };
 
-prompt('What\'s the first number?');
-let num1 = rlSync.question();
-while (num1.trim() === '' || Number.isNaN(Number(num1))) {
-  prompt('Please input a valid number');
-  num1 = rlSync.question();
+  prompt(MESSAGES.num1);
+  let num1 = rlSync.question();
+  while (num1.trim() === '' || Number.isNaN(Number(num1))) {
+    prompt(MESSAGES.invalid_num);
+    num1 = rlSync.question();
+  }
+
+  prompt(MESSAGES.num2);
+  let num2 = rlSync.question();
+  while (num2.trim() === '' || Number.isNaN(Number(num2))) {
+    prompt(MESSAGES.invalid_num);
+    num2 = rlSync.question();
+  }
+
+  prompt(MESSAGES.op);
+  let operation = Number(rlSync.question());
+  while (![1, 2, 3, 4].includes(operation)) {
+    prompt(MESSAGES.invalid_op);
+    operation = Number(rlSync.question());
+  }
+
+  /**
+   * Now that each number has been validated, coerce each one to a number to
+   * avoid
+   * repetitive coercion below.
+   */
+  num1 = Number(num1);
+  num2 = Number(num2);
+
+  /**
+   * Perform the 1 of 4 operations depending on what the user chose.
+   */
+  switch (operation) {
+    case 1:
+      result = num1 + num2;
+      break;
+    case 2:
+      result = num1 - num2;
+      break;
+    case 3:
+      result = num1 * num2;
+      break;
+    default:
+      result = num1 / num2;
+  }
+
+  prompt('Result = ' + result);
+  prompt(MESSAGES.continue);
+  another = rlSync.question();
 }
-
-prompt('What\'s the second number?');
-let num2 = rlSync.question();
-while (num2.trim() === '' || Number.isNaN(Number(num2))) {
-  prompt('Please input a valid number');
-  num2 = rlSync.question();
-}
-
-prompt('What\'s the operation?\nAdd = 1\nSubract = 2\nMultiply = 3\nDivide = 4');
-let operation = Number(rlSync.question());
-while (![1, 2, 3, 4].includes(operation)) {
-  prompt('Please input a valid operation number');
-  operation = Number(rlSync.question());
-}
-
-/**
- * Now that each number has been validated, coerce each one to a number to avoid
- * repetitive coercion below.
- */
-num1 = Number(num1);
-num2 = Number(num2);
-
-/**
- * Perform the 1 of 4 operations depending on what the user chose.
- */
-switch (operation) {
-  case 1:
-    result = num1 + num2;
-    break;
-  case 2:
-    result = num1 - num2;
-    break;
-  case 3:
-    result = num1 * num2;
-    break;
-  default:
-    result = num1 / num2;
-}
-
-prompt('Result = ' + result);
