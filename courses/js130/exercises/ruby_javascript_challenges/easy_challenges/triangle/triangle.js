3a97b8f297f3e95a3538e406479cdeb1669f7d83
anthnyalxndr <58750392+anthnyalxndr@users.noreply.github.com>
2023-06-18 13:09:32 -0700

    Adds local jest install

--- courses/js130/exercises/ruby_javascript_challenges/easy_challenges/triangle/triangle.js
+++ courses/js130/exercises/ruby_javascript_challenges/easy_challenges/triangle/triangle.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 /*
 problem description: Write a program to determine whether a triangle is
 equilateral, isosceles, or scalene.
@@ -13,4 +14,53 @@ the sum of the lengths of any two sides must be greater than the length of the
 third side.
 */
 
+/*
+PEDAC
+- Problem:
+- Examples / Test Cases:
+- DSA:
+*/
+class Triangle {
+  constructor () {
+    const sideLengths = Array.from(arguments);
+    this.sideLengths = sideLengths;
 
+    if (this.sideLengths.some((length) => length <= 0)) {
+      throw new Error("All sides must have a length greater than 0");
+    }
+
+    const violatesTriangleInequality = () => {
+      const [side1Length, side2Length, side3Length] = sideLengths;
+      const sideCombinations = [
+        [[side1Length, side2Length], side3Length],
+        [[side1Length, side3Length], side2Length],
+        [[side2Length, side3Length], side1Length],
+      ];
+      const sum = (num1, num2) => num1 + num2;
+      for (let combo of sideCombinations) {
+        let [[first, second], third] = combo;
+        if (sum(first, second) > third) return true;
+      }
+      return false;
+    };
+
+    const isEquilateral = () => (new Set(sideLengths)).size === 1;
+
+    if (!isEquilateral() && violatesTriangleInequality()) {
+      throw new Error("Violates Triangle Inequality");
+    }
+  }
+  kind() {
+    const triangleCategoryMap = {
+      3: 'scalene',
+      2: 'isoscles',
+      1: 'equilateral',
+    };
+    let uniqueSides = (new Set(this.sideLengths)).size;
+    return triangleCategoryMap[uniqueSides];
+  }
+}
+
+console.log((new Triangle(2, 2, 2).kind()));
+
+module.exports = Triangle;
